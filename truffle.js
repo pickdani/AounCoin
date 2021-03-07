d445a2853e5cb74d080cd62dab283de94c5e66d4
pickdani <me@pickdani.com>
2021-03-07 00:49:12 -0500

    local network config

--- truffle.js
+++ truffle.js
@@ -1,3 +1,12 @@
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
-}
+
+    // how to connect to local blockchain
+    networks: {
+        development: {
+            host: "127.0.0.1"
+            port: "7545"
+            network_id: "*" // match any network id
+        }
+    }
+};
